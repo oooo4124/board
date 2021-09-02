c57f47bda04be923cdb8d4e48db3adeb2eb03895
lsh95 <58044435+lsh95@users.noreply.github.com>
2021-09-02 17:48:54 +0900

    Querydsl 사용, 페이징 추가

--- src/main/java/hello/board/web/controller/PostController.java
+++ src/main/java/hello/board/web/controller/PostController.java
@@ -17,6 +17,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
